1c1568fd1f45ef0a8c3181268eac19c6616ea1be
Andrew Abogado <me@andrewabogado.com>
2015-02-23 01:15:13 +0800

    Remove function AppCtrl and replace it with proper module declaration

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,17 @@
 'use strict';
 
-function AppCtrl ($scope) {
-  $scope.name = "World!";
-  $scope.updateName = function () {
-    $scope.name = "World from function!";
-  }
-}
+var controllers = angular.module('dabbble.controllers', []);
+
+// define controllers as part of the module
+controllers.controller('AppCtrl', function($scope) {
+
+  $scope.name = "Module! To test out module";
+
+});
+
+// function AppCtrl ($scope) {
+//   $scope.name = "World!";
+//   $scope.updateName = function () {
+//     $scope.name = "World from function!";
+//   }
+// }
