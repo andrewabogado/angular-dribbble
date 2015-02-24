cbb946bc1477e652b98a1816162ffc4458f4084d
Andrew Abogado <me@andrewabogado.com>
2015-02-25 01:20:19 +0800

    Add shots list using Dribbble API

--- js/controllers.js
+++ js/controllers.js
@@ -9,9 +9,12 @@ controllers.controller('AppCtrl', function($scope) {
 
 });
 
-// function AppCtrl ($scope) {
-//   $scope.name = "World!";
-//   $scope.updateName = function () {
-//     $scope.name = "World from function!";
-//   }
-// }
+controllers.controller('ShotsListCtrl', function($scope, $http) {
+  $scope.list;
+
+  $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function (data){
+    $scope.list = data.data;
+    console.log(data);
+  })
+
+});
