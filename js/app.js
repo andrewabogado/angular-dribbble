7d9d47c7eb50b460544bed2c0c0ade8015bc9ed5
Andrew Abogado <me@andrewabogado.com>
2015-02-25 02:59:14 +0800

    Separate shots_list.html on partials folder and replace with ng-view; Implement routing for ShotsListCtrl; Add angular-route, bootstrap fonts, and style stats;

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-var app = angular.module('dabbble', ['dabbble.controllers']);
+var app = angular.module('dabbble', ['dabbble.controllers', 'ngRoute']);
+
+app.config(["$routeProvider", function($routeProvider) {
+	$routeProvider
+		.when("/:list", {
+			controller: "ShotsListCtrl",
+			templateUrl: "partials/shots_list.html"
+		})
+		.otherwise({
+			redirectTo: "/popular"
+		});
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -12,9 +12,10 @@ controllers.controller('AppCtrl', function($scope) {
 controllers.controller('ShotsListCtrl', function($scope, $http) {
   $scope.list;
 
-  $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function (data){
-    $scope.list = data.data;
-    console.log(data);
-  })
+  $http.jsonp(
+    'http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(
+    function(data) {
+      $scope.list = data.data;
+    })
 
 });
