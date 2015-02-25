971c5de0c81cab4737521fe21b5d5b1f1ad6be7f
Andrew Abogado <me@andrewabogado.com>
2015-02-25 23:37:21 +0800

    Make the shots route param id and data dynamic

--- js/controllers.js
+++ js/controllers.js
@@ -13,14 +13,23 @@ controllers.controller('ShotsListCtrl', function($scope, $routeParams, $http) {
 
 	var list = $routeParams.list;
 
-	$http.jsonp(
-		'http://api.dribbble.com/shots/' + list + '?callback=JSON_CALLBACK').then(
-		function(data) {
+	$http
+		.jsonp('http://api.dribbble.com/shots/' + list + '?callback=JSON_CALLBACK')
+		.then(function(data) {
 			$scope.list = data.data;
 		})
 
 });
 
-controllers.controller('ShotsCtrl', function($scope, $http) {
+controllers.controller('ShotsCtrl', function($scope, $routeParams, $http) {
+
+	var id = $routeParams.id;
+
+	$http
+		.jsonp('http://api.dribbble.com/shots/' + id + '?callback=JSON_CALLBACK')
+		.then(function(data) {
+			$scope.shot = data.data;
+			console.log(data);
+		})
 
 });
